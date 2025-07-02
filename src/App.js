6cc44b32498c0982eae31c3ecebf274a0e1ceaf1
Daniel <ginzb014@umn.edu>
2025-07-01 21:57:40 -0500

    fixed file structure, mostly finished hero section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-import profilepic from './/assets/profile-pic.JPG';
+import profilepic from './assets/profile-pic.JPG';
+import locpin from './assets/location-pin.png';
+import linkedin from './assets/linkedin.png';
+import github from './assets/github.png';
+import email from './assets/mail.png';
 
 function App() {
   return (
@@ -31,7 +35,24 @@ function App() {
             <p className="hero-subtitle">
               Software and Web <span className="highlight-hero">Developer</span>
             </p>
-            <p className="hero-information">Minneapolis, MN</p>
+            {/* <div className="hero-location">
+              <img src={locpin} alt="Location Icon" className="hero-icon"/>
+              <p className="hero-information">Minneapolis, MN</p>
+            </div> */}
+            <div className="hero-buttons">
+              <a href="https://www.linkedin.com/in/daniel-ginzburg/" target="_blank" rel="noopener noreferrer">
+                <img src={linkedin} alt="LinkedIn Profile" width="32" height="32" />
+                <span className='hero-button-text'>LinkedIn</span>
+              </a>
+              <a href="https://github.com/danginzburg" target="_blank" rel="noopener noreferrer">
+                <img src={github} alt="GitHub Profile" width="32" height="32" />
+                <span className='hero-button-text'>GitHub</span>
+              </a>
+              <a href="mailto:daniel.ginzburg28@gmail.com">
+                <img src={email} alt="Email" width="32" height="32" />
+                <span className='hero-button-text'>Email</span>
+              </a>
+            </div>
           </div>
           <div>
             <img src={profilepic} alt="Profile" className="hero-image"/>
